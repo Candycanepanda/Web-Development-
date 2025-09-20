690d47710ac8b414deaa762346b58200c108b700
Candycanepanda <164298882+Candycanepanda@users.noreply.github.com>
2025-09-20 21:48:57 +0530

    Add files via upload

---
+++ MITBOTS TASK 5/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Header />
+      <Menu />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
